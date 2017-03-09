37d63385d34ada6ad609d3fce0c95cf456934e09
daramful <hlee295@illinois.edu>
2017-03-09 05:56:32 -0600

    Update and rename catering.html to catering.php

--- page/catering.php
+++ page/catering.php
@@ -38,10 +38,43 @@
 <div class="container-fluid dark" id="catering">
     <div class="container">
         <div class="row">
-      		<h3> CATERING: COMING UP </h3>
-        </div>
-    </div>
-</div>    
+      		<h3> CATERING : TEST (not complete yet)</h3>
+
+
+			<?php
+			// Initial form
+			if ($_REQUEST[current] != 1) {
+			?>
+			
+			<h2>Morning Sets</h2>
+			<form method=POST><input type=hidden name=current value=1>
+			<table border=1>
+			<tr><td>Email Address</td><td><input name=email size=4></td></tr>
+			<tr><td>Continental Breakfast</td><td><input name=continental size=4></td></tr>
+			<tr><td>Morning Sandwich Set</td><td><input name=sandwich size=4></td></tr>
+			<tr><td>Fruit Trays</td><td><input name=fruit size=4></td></tr>
+			<tr><td>&nbsp;</td><td><input type=submit value="Inquire"></td></tr>
+			</table></form>
+			
+			<?php
+			} else {
+			
+			// handling results
+			echo "The total price will be mailed to you shortly. Thank you for visiting Deli Biz Baz!";
+			
+			?>
+			<br>
+			Please click <a href=?>here</a> to rerun the price.
+			<br>
+			<?php } ?>
+	
+	    </div>
+	</div>
+</div>
+	
+	<br>
+	<br>
+	
 <footer class="row" id="footer">
     <p><small>Deli Biz Baz &copy; 2014 All Rights Reserved.</small></p>
 </footer>
